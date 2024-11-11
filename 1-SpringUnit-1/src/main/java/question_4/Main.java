454bdd58b66d99d7df7519969647590f08c68fcf
mariyammemon <mariyamm1906@gmail.com>
2024-11-11 09:11:16 +0530

    SpringCore

---
+++ 1-SpringUnit-1/src/main/java/question_4/Main.java
@@ -0,0 +1,28 @@
+package question_4;
+//4. Write a code to print student id name answer by asking question using collection of constructor injection of spring Dependency Injection.
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("question_4/applicationContext.xml");
+        Student student = (Student) context.getBean("student");
+        System.out.println("Student ID: " + student.getId());
+        System.out.println("Student Name: " + student.getName());
+        for (Question question : student.getQuestions()) {
+            System.out.println("Question ID: " + question.getId());
+            System.out.println("Question: " + question.getQuestion());
+            System.out.println("Answers: " + question.getAnswers());
+        }
+    }
+}
+
+//Student ID: 22000214
+//Student Name: Abhiraj
+//Question ID: 1
+//Question: What is Java?
+//Answers: [Programming Language, Platform, Both]
+//Question ID: 2
+//Question: What is Spring?
+//Answers: [Framework, Library, Both]
